4a7f270eab0b7b01384e6e43c9ef24c998c6ed0f
Abdi begna <abditadesse89@gmail.com>
2026-04-21 23:52:01 +0300

    Comment out the tickets array and its export in database.ts

--- app/database.ts
+++ app/database.ts
@@ -1,66 +1,66 @@
-const tickets = [
-  { id: 1, name: "Fix next,js API Route bug", status: "open", type: "bug" },
-  {
-    id: 2,
-    name: "Add a new feature to the app",
-    status: "open",
-    type: "feature",
-  },
-  {
-    id: 3,
-    name: "Refactor the app",
-    status: "open",
-    type: "refactor",
-  },
-  { id: 4, name: "Fix a bug in the app", status: "open", type: "bug" },
-  { id: 5, name: "Add new feture to the app", status: "open", type: "feature" },
-  {
-    id: 6,
-    name: "Refactor the app",
-    status: "open",
-    type: "refactor",
-  },
-  { id: 7, name: "Fix a bug in the app", status: "open", type: "bug" },
-  { id: 8, name: "Add new feture to the app", status: "open", type: "feature" },
-  {
-    id: 9,
-    name: "Refactor the app",
-    status: "open",
-    type: "refactor",
-  },
-  { id: 10, name: "Fix a bug in the app", status: "open", type: "bug" },
-  {
-    id: 11,
-    name: "Add new feture to the app",
-    status: "open",
-    type: "feature",
-  },
-  {
-    id: 12,
-    name: "Refactor the app",
-    status: "open",
-    type: "refactor",
-  },
-  { id: 13, name: "Fix a bug in the app", status: "open", type: "bug" },
-  {
-    id: 14,
-    name: "Add new feture to the app",
-    status: "open",
-    type: "feature",
-  },
-  {
-    id: 15,
-    name: "Refactor the app",
-    status: "open",
-    type: "refactor",
-  },
-  { id: 16, name: "Fix a bug in the app", status: "open", type: "bug" },
-  {
-    id: 17,
-    name: "Add new feture to the app",
-    status: "open",
-    type: "feature",
-  },
-];
+// const tickets = [
+//   { id: 1, name: "Fix next,js API Route bug", status: "open", type: "bug" },
+//   {
+//     id: 2,
+//     name: "Add a new feature to the app",
+//     status: "open",
+//     type: "feature",
+//   },
+//   {
+//     id: 3,
+//     name: "Refactor the app",
+//     status: "open",
+//     type: "refactor",
+//   },
+//   { id: 4, name: "Fix a bug in the app", status: "open", type: "bug" },
+//   { id: 5, name: "Add new feture to the app", status: "open", type: "feature" },
+//   {
+//     id: 6,
+//     name: "Refactor the app",
+//     status: "open",
+//     type: "refactor",
+//   },
+//   { id: 7, name: "Fix a bug in the app", status: "open", type: "bug" },
+//   { id: 8, name: "Add new feture to the app", status: "open", type: "feature" },
+//   {
+//     id: 9,
+//     name: "Refactor the app",
+//     status: "open",
+//     type: "refactor",
+//   },
+//   { id: 10, name: "Fix a bug in the app", status: "open", type: "bug" },
+//   {
+//     id: 11,
+//     name: "Add new feture to the app",
+//     status: "open",
+//     type: "feature",
+//   },
+//   {
+//     id: 12,
+//     name: "Refactor the app",
+//     status: "open",
+//     type: "refactor",
+//   },
+//   { id: 13, name: "Fix a bug in the app", status: "open", type: "bug" },
+//   {
+//     id: 14,
+//     name: "Add new feture to the app",
+//     status: "open",
+//     type: "feature",
+//   },
+//   {
+//     id: 15,
+//     name: "Refactor the app",
+//     status: "open",
+//     type: "refactor",
+//   },
+//   { id: 16, name: "Fix a bug in the app", status: "open", type: "bug" },
+//   {
+//     id: 17,
+//     name: "Add new feture to the app",
+//     status: "open",
+//     type: "feature",
+//   },
+// ];
 
-export default tickets;
+// export default tickets;
