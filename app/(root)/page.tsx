338b66a26f9e26b2211918e0fe403c6034271f4c
Abdi begna <abditadesse89@gmail.com>
2026-04-30 19:38:32 +0300

    Remove commented-out code for questions and filtering logic in Home component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,47 +17,7 @@ import Link from "next/link";
 interface SearchParams {
   searchParams: Promise<{ [Key: string]: string }>;
 }
-// const questions = [
-//   {
-//     _id: "1",
-//     title: "What is React?",
-//     description:
-//       "I'm new to frontend development. Can someone explain what React is and why it's so popular?",
-//     tags: [
-//       { _id: "1", name: "React" },
-//       { _id: "2", name: "JavaScript" },
-//     ],
-//     author: {
-//       _id: "1",
-//       name: "John Doe",
-//       image:
-//         "https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-174669.jpg",
-//     },
-//     upvotes: 10,
-//     answers: 5,
-//     views: 100,
-//     createdAt: new Date(),
-//   },
-//   {
-//     _id: "2",
-//     title: "How to learn JavaScript?",
-//     description: "I want to learn JavaScript, can anyone help me?",
-//     tags: [
-//       { _id: "2", name: "JavaScript" },
-//       { _id: "1", name: "React" },
-//     ],
-//     author: {
-//       _id: "1",
-//       name: "John Doe",
-//       image:
-//         "https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-174669.jpg",
-//     },
-//     upvotes: 10,
-//     answers: 5,
-//     views: 100,
-//     createdAt: new Date(),
-//   },
-// ];
+
 // const test = async () => {
 //   try {
 //     // await dbConnect();
@@ -85,15 +45,7 @@ export default async function Home({ searchParams }: SearchParams) {
   });
 
   const { questions } = data || {};
-  // const filteredQuestions = questions.filter((question) => {
-  //   const matchesQuery = question.title
-  //     .toLowerCase()
-  //     .includes(query.toLowerCase());
-  //   const matchesFilter = filter
-  //     ? question.tags[0].name.toLowerCase() === filter
-  //     : true;
-  //   return matchesQuery && matchesFilter;
-  // });
+
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
